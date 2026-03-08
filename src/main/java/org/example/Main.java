de4e31b72fe67e5b0452e224b2498026ef271ddb
serayilmaz <serayilmaz96@gmail.com>
2026-03-09 02:03:28 +0300

    git commit -m Implement required Java methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,37 @@
 package org.example;
-
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        }
+        return barking && (hourOfDay < 8 || hourOfDay >= 20);
     }
 
-    public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+    public static boolean hasTeen(int a, int b, int c) {
+        return (a >= 13 && a <= 19) ||
+                (b >= 13 && b <= 19) ||
+                (c >= 13 && c <= 19);
     }
 
-    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer) {
+            return temperature >= 25 && temperature <= 45;
+        }
+        return temperature >= 25 && temperature <= 35;
     }
 
-    public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
-    }
-
-    public static double area(double width, double height) {
-       
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1;
+        }
+        return x * y;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
     }
 }
